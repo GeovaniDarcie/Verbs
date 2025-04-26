903eebb8ed88c4b9672eef5507e0354afcc3b66d
Geovani Darcie <geovani_darcie@hotmail.com>
2025-04-25 21:37:18 -0300

    correcao 234

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ document.getElementById("lampada").addEventListener("click", (e) => {
 
 document.getElementById("autofalante").addEventListener("click", async () => {
   const word = document.getElementById("verbo").innerText.toLowerCase();
-  const url = `https://wordnik-5lg94nc7u-geovani-darcies-projects.vercel.app/api/audio?word=${word}`;
+  const url = `https://wordnik-beeulxrm2-geovani-darcies-projects.vercel.app/api/audio?word=${word}`;
   const audioDiv = document.getElementById('autofalante');
   try {
     const response = await fetch(url);
